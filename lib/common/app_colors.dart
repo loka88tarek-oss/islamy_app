568f247ed9a64165e50d933eb01c0d03dc89b989
Malak <loka88tarek@gmail.com>
2026-09-12 15:08:45 +0300

    create splash screen

--- lib/common/app_colors.dart
+++ lib/common/app_colors.dart
@@ -2,5 +2,5 @@ import 'dart:ui';
 
 class AppColors {
   static const Color goldColor = Color(0xFFE2BE7F);
-  static const Color blackColor = Color(0xFF000000);
+  static const Color blackColor = Color(0xFF212121);
 }
